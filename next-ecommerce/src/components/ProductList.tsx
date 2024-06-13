e72af87faa6d1c97a90b70e86697c51b416edb40
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-06-13 23:24:00 +0700

    product list design

---
+++ next-ecommerce/src/components/ProductList.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex gap-x-8 gay-y-16 justify-between flex-wrap">
+      <Link
+        href="/test"
+        className="relative w-full h-80 flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="/logo.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="/cart.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$44</span>
+        </div>
+        <div className="text-sm text-gray-500">DESC</div>
+        <button className="rounded-2xl ring-1 ring-[#F35C7A] text-[#F35C7A] w-max py-2 px-4 text-xs hover:bg-[#F35C7A] hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
--- next-ecommerce/src/components/Slider.tsx
+++ next-ecommerce/src/components/Slider.tsx
@@ -81,7 +81,7 @@ const Slider = () => {
       <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
         {slides.map((slide, index) => (
           <div
-            className={`w-3 h-3  rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+            className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
               current === index ? "scale-150" : ""
             }`}
             key={slide.id}
