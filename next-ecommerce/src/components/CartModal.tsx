79a3c462fc5a1a697c2809d7d536ea6b6dc0e896
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-06-13 14:04:37 +0700

    fix padding navbar and design cart component

--- next-ecommerce/src/components/CartModal.tsx
+++ next-ecommerce/src/components/CartModal.tsx
@@ -1,7 +1,65 @@
 "use client";
 
+import Image from "next/image";
+
 const CartModal = () => {
-  return <div>CartModal</div>;
+  const cartItems = true;
+
+  return (
+    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+      {!cartItems ? (
+        <div>Cart is Empty</div>
+      ) : (
+        <>
+          <h2 className="text-lg">Shopping Cart</h2>
+          <div className="flex flex-col gap-8">
+            <div className="flex gap-4">
+              <Image
+                src="/logo.png"
+                alt="cart"
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+
+              <div className="flex flex-col justify-between w-full">
+                {/* TOP */}
+                <div>
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$22</div>
+                  </div>
+                  <div className="text-sm text-gray-500">available</div>
+                </div>
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* BOTTOM */}
+          <div>
+            <div className="flex items-center justify-between font-semibold">
+              <span>Subtotal</span>
+              <span>$29</span>
+            </div>
+            <p className="text-gray-500 text-xs mt-2 mb-4">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit
+            </p>
+            <div className="flex justify-between text-sm">
+              <button className="rounded-md py-3 px-4 ring-1 ring-gray-300 ">
+                View Cart
+              </button>
+              <button className="rounded-md py-3 px-4 bg-black text-white">
+                Checkout
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default CartModal;
