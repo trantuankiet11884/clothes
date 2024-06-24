ebce09fc476341261d5e06bdf7b96b528ebdb4b3
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-06-24 09:27:48 +0700

    wix headless js sdk

---
+++ next-ecommerce/src/context/wixContext.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { createClient, OAuthStrategy } from "@wix/sdk";
+import { products, collections } from "@wix/stores";
+import Cookies from "js-cookie";
+import { ReactNode, createContext } from "react";
+
+const refreshToken = JSON.parse(Cookies.get("refreshToken") || "{}");
+
+const WixClient = createClient({
+  modules: {
+    products,
+  },
+  auth: OAuthStrategy({
+    clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+    tokens: {
+      refreshToken,
+      accessToken: { value: "", expiresAt: 0 },
+    },
+  }),
+});
+
+export type WixClient = typeof WixClient;
+
+export const WixClientContext = createContext<WixClient>(WixClient);
+
+export const WixClientContextProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  return (
+    <WixClientContext.Provider value={WixClient}>
+      {children}
+    </WixClientContext.Provider>
+  );
+};
