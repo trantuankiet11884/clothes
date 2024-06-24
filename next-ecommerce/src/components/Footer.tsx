ad6b2a70c076647608b5406f5a950e9dcde73056
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-06-24 07:47:16 +0700

    color p cofnig

--- next-ecommerce/src/components/Footer.tsx
+++ next-ecommerce/src/components/Footer.tsx
@@ -68,7 +68,7 @@ const Footer = () => {
               placeholder="Email address"
               className="p-4 w-3/4"
             />
-            <button className="w-1/4 bg-lama text-white">JOIN</button>
+            <button className="w-1/4 bg-p text-white">JOIN</button>
           </div>
           <span className="font-semibold">Secure Payments</span>
           <div className="flex justify-between">
